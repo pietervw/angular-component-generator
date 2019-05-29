68a785e1782341840759289036b4b26014186421
Pieter <pietervw@posteo.net>
2019-05-29 15:38:04 +0200

    improved readiblity

--- ng g.bat
+++ ng g.bat
@@ -1,18 +1,21 @@
 @echo off
 :BEGIN
+
 echo.
 SET /P TYPE=What type of component are you generating (c, s, p, d,...)? 
+
 echo.
 echo Enter the path. Path name ends with component name. Example:
 echo app\ui\technical\users\UsersTable
+
 echo.
 SET /P FOLDERPATH=Path? app\
 SET command=npm run ng g %TYPE% %FOLDERPATH% %NAME%
 
 echo.
 echo %command%
-echo.
 
+echo.
 SET /P AREYOUSURE=Are you sure you want to execute this command (y/n)? 
 IF /I "%AREYOUSURE%" NEQ "y" GOTO END
 
